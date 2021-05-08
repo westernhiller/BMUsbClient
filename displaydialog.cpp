b7861443ec79dfd47e9287197a4eb4f260b860c4
westernhiller <zhangwayne@hotmail.com>
2021-05-08 11:37:03 +0800

    Update displaydialog.cpp
    added FPS

--- displaydialog.cpp
+++ displaydialog.cpp
@@ -3,6 +3,8 @@
 
 DisplayDialog::DisplayDialog(QWidget *parent)
     : QDialog(parent)
+    , m_nFrames(0)
+    , m_fps("FPS:0")
 {
     QHBoxLayout* playout = new QHBoxLayout();
     m_pCanvas = new Canvas();
@@ -10,10 +12,21 @@ DisplayDialog::DisplayDialog(QWidget *parent)
     setLayout(playout);
 
     connect(this, SIGNAL(displayImage(QImage)), m_pCanvas, SLOT(updateImage(QImage)));
+    m_pTimer = new QTimer();
+    connect(m_pTimer, SIGNAL(timeout()), this, SLOT(onTimer()));
+    m_pTimer->start(1000);
 }
 
 void DisplayDialog::display(cv::Mat image)
 {
     resize(image.cols, image.rows);
+    m_nFrames++;
+    cv::putText(image, m_fps.toStdString(), cv::Point(50, 50), cv::FONT_HERSHEY_COMPLEX, 2, cv::Scalar(0, 255, 255), 2);
     emit displayImage(mat2qimage(image));
+}
+
+void DisplayDialog::onTimer()
+{
+    m_fps = QString("FPS:%1").arg(m_nFrames);
+    m_nFrames = 0;
 }
